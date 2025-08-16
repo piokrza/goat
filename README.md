ed027082ae310234de58d813c2f43dfcff34edf1
Piotr Krzaczkowski <slimhannya@gmail.com>
2025-08-16 13:27:11 +0200

    feat: generate web & widget apps

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Shell
+# Echo
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.1.
 
---
+++ projects/web/src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { routes } from './app.routes';
+
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection(), provideRouter(routes)],
+};
