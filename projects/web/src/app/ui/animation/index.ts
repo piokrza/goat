89f35087dd5bd6e232cdc75bafcfeb63c2fa7eeb
Piotr Krzaczkowski <slimhannya@gmail.com>
2025-11-21 17:56:10 +0100

    chore: remote unnecessary animation files

--- projects/web/src/app/ui/animation/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './expand-collapse';
-export * from './route-transition';
--- projects/web/src/app/ui/animation/route-transition.ts
+++
@@ -1,8 +0,0 @@
-import { animate, AnimationTriggerMetadata, query, style, transition, trigger } from '@angular/animations';
-
-export const routeTransition: AnimationTriggerMetadata = trigger('routeTransition', [
-  transition('* => *', [
-    query(':enter', [style({ opacity: 0 })], { optional: true }),
-    query(':enter', [animate('150ms', style({ opacity: 1 }))], { optional: true }),
-  ]),
-]);
